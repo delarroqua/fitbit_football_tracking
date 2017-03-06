dcf97d2ba533bef4c1fb58f0dcaf7df8f5297b0a
Pierre Delarroqua <pierre.delarroqua@gmail.com>
2017-03-06 14:46:35 +0100

    add variable coords_array in js file and improve layout and style with bootstrap

--- static/js/heat_map_google.js
+++ static/js/heat_map_google.js
@@ -2,27 +2,37 @@
 
  function initMap() {
    map = new google.maps.Map(document.getElementById('map'), {
-     zoom: 19,
+     //zoom: 19,
      center: {lat: center_map['lat'], lng: center_map['lng']},
      //center: {lat: 48.831485, lng: 2.272656},
      //center: {lat: 48.874586, lng: 2.277218},
      mapTypeId: 'satellite'
    });
 
+   var coords_array = getPoints()
+
    heatmap = new google.maps.visualization.HeatmapLayer({
-     data: getPoints(),
+     data: coords_array,
      map: map,
-     radius: 11
-     //opacity: 2
+     radius: 11,
+     opacity: 1
    });
+
+   var bounds = new google.maps.LatLngBounds();
+   for (var i = 0; i < coords_array.length; i++) {
+      bounds.extend(coords_array[i]);
+   }
+   map.fitBounds(bounds);
  }
 
  function changeRadius() {
+    // heatmap.set('radius', heatmap.get('radius') ? 20 : heatmap.get('radius'));
     heatmap.set('radius', heatmap.get('radius') ? null : 20);
  }
 
  function changeOpacity() {
-     heatmap.set('opacity', heatmap.get('opacity') ? null : 0.2);
+     // heatmap.set('opacity', heatmap.get('opacity') ? 0.1 : heatmap.get('opacity'));
+     heatmap.set('opacity', heatmap.get('opacity') ? null : 0.1);
  }
 
  function getPoints() {
